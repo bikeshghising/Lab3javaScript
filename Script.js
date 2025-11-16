2f16e12b7776f21cb1861db3c48fdef6f55aad6f
bikeshghising <tmgbikesh@gmail.com>
2025-11-16 11:22:09 -0500

    DOM element references for price, color, and status and connect script file to index.

--- Script.js
+++ Script.js
@@ -4,3 +4,8 @@ const toyCar = {
   color: "Red",
   price: 24.99
 };
+
+// DOM elements
+const priceEl = document.getElementById("price");
+const colorEl = document.getElementById("colorDisplay");
+const statusEl = document.getElementById("status");
